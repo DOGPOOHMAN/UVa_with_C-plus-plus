48fb46b44fe1476af2e0d78caf3a39b17369a41b
Rick-Lee <li.dogpooh@gmail.com>
2017-04-17 16:42:05 +0800

    fix bug uva_11344.cpp

--- uva_11344.cpp
+++ uva_11344.cpp
@@ -7,14 +7,34 @@ using namespace std;
 
 int main(){
     unsigned long long int M = 0;
-    string lotsN;
-    int N, group;
+    string line;
+    int val, group;
+    bool divi_flag;//true:所有數值都能整除  false:無法整除
     
     cin >> group;
     for (int i = 0; i < group; i++) {
         
         cin >> M;
-        cout << M;
+		
+    	getline(cin , line);//把前一行的換行LF讀進來，以免影響後面的作業
+    	getline(cin , line);//讀取整行文字
+        istringstream val_set_stream(line);//分解這行文字為一個個的單字
+
+        divi_flag = true;
+        while(val_set_stream >> val){
+
+        	if((M % val) != 0){
+        		divi_flag = false;
+        		break;
+        	}
+        }
+
+        //printing result
+        cout << M << " - ";
+        if(divi_flag)
+        	cout << "Wonderful.\n";
+        else
+        	cout << "Simple.\n";
     }
     
     return 0;
